4aa0b5041e128c7da9c95de425bbea4df626c365
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 01:52:05 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
 test(t => {
-	t.is(fn('sindresorhus@gmail.com', {size: 200}), 'https://gravatar.com/avatar/d36a92237c75c5337c17b60d90686bf9?size=200');
-	t.end();
+	t.is(m('sindresorhus@gmail.com', {size: 200}), 'https://gravatar.com/avatar/d36a92237c75c5337c17b60d90686bf9?size=200');
 });
